69f3d15072694a70d809beab5bb88c5816224b2f
Luís Gustavo <gustavovacodar@gmail.com>
2023-09-05 09:16:03 -0300

    create seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// use App\Models\Seller;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\Seller::create([
+            'name' => 'Test User',
+            'email' => 'test@email.com',
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
